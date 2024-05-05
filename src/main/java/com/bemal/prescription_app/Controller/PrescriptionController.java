e98b9fa3b9c34e086309d40fa0a0ff0a259ef10d
bemal-engenuityai <141420637+bemal-engenuityai@users.noreply.github.com>
2024-05-05 19:46:34 +0530

    added api to prescription

--- src/main/java/com/bemal/prescription_app/Controller/PrescriptionController.java
+++ src/main/java/com/bemal/prescription_app/Controller/PrescriptionController.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/prescription")
+@RequestMapping("/api/prescription")
 @CrossOrigin(origins = "*")
 public class PrescriptionController {
 
